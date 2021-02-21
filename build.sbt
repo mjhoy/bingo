66048caa263d0af1c5a86343c0a0bdb2e38cceef
Michael Hoy <mjh@mjhoy.com>
2021-02-21 09:06:53 -0500

    Laminar hello world

--- build.sbt
+++ build.sbt
@@ -3,5 +3,10 @@ enablePlugins(ScalaJSPlugin)
 name := "Bingo"
 scalaVersion := "2.13.4"
 
+libraryDependencies ++= Seq(
+  "com.raquo" %%% "laminar" % "0.11.0",
+  "org.scala-js" %%% "scalajs-dom" % "1.1.0"
+)
+
 scalaJSUseMainModuleInitializer := true
 mainClass in Compile := Some("bike.mikey.bingo.App")
--- src/main/scala/bike/mikey/bingo/App.scala
+++ src/main/scala/bike/mikey/bingo/App.scala
@@ -1,7 +1,18 @@
 package bike.mikey.bingo
 
+import org.scalajs.dom
+import com.raquo.laminar.api.L._
+
 object App {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    println("Hi from the console!")
+    dom.document.addEventListener(
+      "DOMContentLoaded",
+      { (e: dom.Event) =>
+        val appContainer = dom.document.querySelector("#appContainer")
+        val appElement = div(h1("Hello, world!"))
+        render(appContainer, appElement)
+      }
+    )
   }
 }
