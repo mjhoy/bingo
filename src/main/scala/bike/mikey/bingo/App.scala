6c40cecfe78fd6aaf6a445a35faa27c5b9ed6283
Michael Hoy <mjh@mjhoy.com>
2021-02-24 06:46:48 -0500

    Game.isBingo

--- src/main/scala/bike/mikey/bingo/App.scala
+++ src/main/scala/bike/mikey/bingo/App.scala
@@ -20,7 +20,7 @@ object App {
             case Square(text, picked) => if (t.id == tileId) Square(text, !picked) else t
           },
         )
-        Game(won = isBingo(withPicked), tiles = withPicked)
+        Game(won = Game.isBingo(withPicked), tiles = withPicked)
       })
 
     // calculate if we won
@@ -147,30 +147,5 @@ object App {
         render(appContainer, node)
       },
     )
-  }
-
-  def connected(tileIdx: Int, step: Int => Int, limit: Int, tiles: List[Tile]): Boolean = {
-    if (tileIdx > limit) {
-      return true
-    }
-    val tile = tiles(tileIdx)
-    if (!tile.active) {
-      return false
-    }
-
-    connected(step(tileIdx), step, limit, tiles)
-  }
-
-  def isBingo(tiles: List[Tile]): Boolean = {
-    List(
-      // Horizontal
-      List(0, 5, 10, 15, 20).exists(idx => connected(idx, (_ + 1), idx + 4, tiles)),
-      // Vertical
-      List(0, 1, 2, 3, 4).exists(idx => connected(idx, (_ + 5), 24, tiles)),
-      // Diag left -> right
-      connected(0, (_ + 6), 24, tiles),
-      // Diag right -> left
-      connected(4, (_ + 4), 20, tiles),
-    ).exists(identity)
   }
 }
--- src/main/scala/bike/mikey/bingo/models/Game.scala
+++ src/main/scala/bike/mikey/bingo/models/Game.scala
@@ -1,3 +1,30 @@
 package bike.mikey.bingo.models
 
 case class Game(won: Boolean, tiles: List[Tile])
+
+object Game {
+  private def connected(tileIdx: Int, step: Int => Int, limit: Int, tiles: List[Tile]): Boolean = {
+    if (tileIdx > limit) {
+      return true
+    }
+    val tile = tiles(tileIdx)
+    if (!tile.active) {
+      return false
+    }
+
+    connected(step(tileIdx), step, limit, tiles)
+  }
+
+  def isBingo(tiles: List[Tile]): Boolean = {
+    List(
+      // Horizontal
+      List(0, 5, 10, 15, 20).exists(idx => connected(idx, (_ + 1), idx + 4, tiles)),
+      // Vertical
+      List(0, 1, 2, 3, 4).exists(idx => connected(idx, (_ + 5), 24, tiles)),
+      // Diag left -> right
+      connected(0, (_ + 6), 24, tiles),
+      // Diag right -> left
+      connected(4, (_ + 4), 20, tiles),
+    ).exists(identity)
+  }
+}
